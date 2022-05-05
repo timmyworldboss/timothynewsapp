46579628619493897e669087b29e0763b75efdd2
timmy <timothy.ndung'u@student.moringaschool.com>
2022-05-05 10:40:42 +0300

    request.py

--- app/request.py
+++ app/request.py
@@ -1,10 +1,10 @@
 from app import app
 import urllib.request,json
-from .models import news
+# from .models import news
 
 News = news.News
 # Getting api key
-api_key = app.config['NEWS_API_KEY']
+# api_key = app.config['NEWS_API_KEY']
 # Getting the movie base url
 base_url = app.config["NEWS_API_BASE_URL"]
 
