1b9cb6237d12fc64fce87dae24217b09b1fabc49
timmy <timothy.ndung'u@student.moringaschool.com>
2022-05-05 10:36:10 +0300

    setting up configuration

--- app/__init__.py
+++ app/__init__.py
@@ -6,7 +6,7 @@ from .config import DevConfig
 # app = Flask(__name__,instance_relative_config = True)
 
 # Setting up configuration
-# app.config.from_object(DevConfig)
-# app.config.from_pyfile('config.py')
+app.config.from_object(DevConfig)
+app.config.from_pyfile('config.py')
 
-from app import views
+# from app import views
